9f12017d1b53171ae66f00b74afb4d5089a80dd5
정재웅 <119780428+chachapapa@users.noreply.github.com>
2023-10-21 21:28:51 +0900

    Create 같은 숫자는 싫어.js

---
+++ 같은 숫자는 싫어.js
@@ -0,0 +1,14 @@
+function solution(arr)
+{
+    var answer = [];
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    for(let i = 0 ; i<arr.length ; i++){
+        if(arr[i] !== arr[i+1]){
+            answer.push(arr[i])
+        }
+    }
+    console.log('Hello Javascript')
+    
+    return answer;
+}
